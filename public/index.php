9eca98c2c7529cea6600686a4f06aff6f58b1dee
YoussefIbraheem <youssefm.muhammed@gmail.com>
2024-10-15 16:28:21 +0300

    adjust cards and include layout

--- public/index.php
+++ public/index.php
@@ -5,18 +5,23 @@ use App\Layouts\Card;
 use App\Layouts\Footer;
 use App\Layouts\Header;
 use App\Layouts\Navbar;
+use App\Layouts\ProductList;
 
-(new Header('Scandiweb product List'))->render();
+$products = [
+    ['id' => 1, 'title' => 'Product 1', 'body' => 'Description for Product 1'],
+    ['id' => 2, 'title' => 'Product 2', 'body' => 'Description for Product 2'],
+    ['id' => 3, 'title' => 'Product 3', 'body' => 'Description for Product 3'],
+    ['id' => 1, 'title' => 'Product 1', 'body' => 'Description for Product 1'],
+    ['id' => 2, 'title' => 'Product 2', 'body' => 'Description for Product 2'],
+    ['id' => 3, 'title' => 'Product 3', 'body' => 'Description for Product 3'],
+    ['id' => 1, 'title' => 'Product 1', 'body' => 'Description for Product 1'],
+    ['id' => 2, 'title' => 'Product 2', 'body' => 'Description for Product 2'],
+    ['id' => 3, 'title' => 'Product 3', 'body' => 'Description for Product 3'],
+];
+
+
+(new Header('Scandiweb product List 2'))->render();
 
 (new Navbar(['About'=>'https://google.com']))->render();
-
-echo "<div class='container mt-4'>
-        <div class='row'>";
-
-for($i = 0 ; $i <= 5 ; $i++) {
-    (new Card($i,"Title {$i}", "Body {$i}"))->render();
-}
-
-echo "</div></div>";
-
+(new ProductList($products))->render();
 (new Footer)->render();
