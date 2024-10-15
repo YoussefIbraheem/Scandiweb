ca84d4660891b9e36f2f64d100b4683b92e24031
YoussefIbraheem <youssefm.muhammed@gmail.com>
2024-10-15 22:20:48 +0300

    adjust class namings

---
+++ src/Views/Card.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Views;
+
+class Card extends Layout
+{
+  private array $data;
+
+
+  public function __construct($id, $title, $body)
+  {
+    $this->data = [
+      'id' => $id,
+      'title' => $title,
+      'body' => $body
+    ];
+  }
+
+  public function render()
+  {
+    self::renderTemplate('views/card', $this->data);
+  }
+}
